5f0a78cb16af96be29ba86827fcd0009f99c28ee
Dangermannn <dangermanplyt@gmail.com>
2020-04-17 17:17:58 +0200

    Fixed not appearing bricks

--- src/GameInitialisation.py
+++ src/GameInitialisation.py
@@ -3,18 +3,21 @@ import pygame
 
 screen = pygame.display.set_mode((750, 750))
 background = pygame.image.load('Images/Mapv2.png')
-
+stone = pygame.image.load('Images/stone.png')
 pygame.display.set_caption('Bomberman')
 icon = pygame.image.load('Images/gosc124.png')
 pygame.display.set_icon(icon)
 
+game_map = []
+BLOCK_SIZE = 50
+
 game_map_str =  """\
 ###############
 #   # # # #   #
-# #         # #
-#   # # # #   #
-## ## # # ## ##
-#             #
+# #   SSS   #S#
+#   #S#S#S#SSS#
+## ##S#S#S##S##
+#   S         #
 # # ####### # #
 #             #
 # ### # # ### #
@@ -26,10 +29,21 @@ game_map_str =  """\
 ###############
 """
 
-game_map = []
 
-for line_str in game_map_str.splitlines():
-	game_map.append(list(line_str))
-	
+def generateMap(game_map):
+	for line_str in game_map_str.splitlines():
+		game_map.append(list(line_str))
 
+stoneBlocks = []
+#generateMap(game_map)
 
+def placeStones():
+	for i in range(len(game_map)):
+		for j in range(len(game_map[i])):
+			if game_map[i][j] == 'S':
+				#stoneBlocks.append((i, j))
+				screen.blit(stone, (BLOCK_SIZE * i, BLOCK_SIZE * j))
+
+def drawMap():
+	for x in game_map:
+		print(x)
